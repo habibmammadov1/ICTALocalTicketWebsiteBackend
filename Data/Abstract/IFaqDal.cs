212e64cd6063bb97bc369c959a5da7ff18a9c80c
Nigar <haciyeva.nigar2004@gmail.com>
2025-11-04 19:07:30 +0400

    Added FAQ module

---
+++ Data/Abstract/IFaqDal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.DataAccess;
+using Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Data.Abstract
+{
+    public interface IFaqDal
+    {
+        Task<List<Faq>> GetAllAsync();
+        Task<Faq> GetByIdAsync(int id);
+        Task AddAsync(Faq faq);
+        Task UpdateAsync(Faq faq);
+        Task DeleteAsync(int id);
+    }
+}
+
+
---
+++ Data/Concrete/EfFaqDal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.DataAccess.EntityFramework;
+using Data.Abstract;
+using DataAccess.Abstract;
+using Entities;
+
+namespace Data.Concrete
+{
+    public class EfFaqDal : EfEntityRepositoryBase<Faq, AppDbContext>, IFaqDal
+    {
+    }
+}
+
